e67b06f8b50a0d6014a54f446323d665f8292aa3
Jonathan Cruz <jcruzdeveloper@gmail.com>
2018-02-04 21:50:51 -0800

    routes: scope api endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resource :balances, only: [:show, :update]
-  resource :tickers, only: [:show]
-  resources :users, except: [:index, :update, :destroy]
-  post 'user_token' => 'user_token#create'
+  scope 'v1' do
+    resource :balances, only: [:show, :update]
+    resource :tickers, only: [:show]
+    resources :users, except: [:index, :update, :destroy]
+    post 'user_token' => 'user_token#create'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
