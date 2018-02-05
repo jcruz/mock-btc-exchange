6cae9f59d3b4143b771a5a5e24a7ee3607640f9c
Jonathan Cruz <jcruzdeveloper@gmail.com>
2018-02-04 21:31:51 -0800

    controllers: users: create with email & password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 
   # POST /users
   def create
-    @user = User.new(user_params)
+    @user = User.new(email: params['email'], password: params['password'])
 
     if @user.save
       render json: @user, status: :created, location: @user
