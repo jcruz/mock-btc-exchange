21739f78087cb3cd26d59d037ba850ba512897f9
Jonathan Cruz <jcruzdeveloper@gmail.com>
2018-02-06 21:52:41 -0800

    test: fix integration tests

--- test/controllers/balances_controller_test.rb
+++ test/controllers/balances_controller_test.rb
@@ -5,34 +5,13 @@ class BalancesControllerTest < ActionDispatch::IntegrationTest
     @balance = balances(:one)
   end
 
-  test "should get index" do
+  test "should show balance unauthorized" do
     get balances_url, as: :json
-    assert_response :success
+    assert_response :unauthorized
   end
 
-  test "should create balance" do
-    assert_difference('Balance.count') do
-      post balances_url, params: { balance: { btc: @balance.btc, usd: @balance.usd, user_id: @balance.user_id } }, as: :json
-    end
-
-    assert_response 201
-  end
-
-  test "should show balance" do
-    get balance_url(@balance), as: :json
-    assert_response :success
-  end
-
-  test "should update balance" do
-    patch balance_url(@balance), params: { balance: { btc: @balance.btc, usd: @balance.usd, user_id: @balance.user_id } }, as: :json
-    assert_response 200
-  end
-
-  test "should destroy balance" do
-    assert_difference('Balance.count', -1) do
-      delete balance_url(@balance), as: :json
-    end
-
-    assert_response 204
+  test "should update balance unauthorized" do
+    patch balances_url, params: { balance: { btc: @balance.btc, usd: @balance.usd, user_id: @balance.user_id } }, as: :json
+    assert_response :unauthorized
   end
 end
